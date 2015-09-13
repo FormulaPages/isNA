4e4a61fad631e30e1480346100b7f7577fff95de
Peter Moresi <peter.moresi@gmail.com>
2015-09-12 17:29:47 -0700

    first commit

--- ISNA.es6
+++ ISNA.es6
@@ -1,4 +1,6 @@
 
+import error from 'formula-errors';
+
 export default function isNA(value) {
        return value === error.na;
 }
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 (function (global, factory) {
-       if (typeof define === "function" && define.amd) {
-              define(["exports", "module"], factory);
-       } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
-              factory(exports, module);
+       if (typeof define === 'function' && define.amd) {
+              define(['exports', 'module', 'formula-errors'], factory);
+       } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+              factory(exports, module, require('formula-errors'));
        } else {
               var mod = {
                      exports: {}
               };
-              factory(mod.exports, mod);
+              factory(mod.exports, mod, global.error);
               global.ISNA = mod.exports;
        }
-})(this, function (exports, module) {
-       "use strict";
+})(this, function (exports, module, _formulaErrors) {
+       'use strict';
 
        module.exports = isNA;
 
+       function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+       var _error = _interopRequireDefault(_formulaErrors);
+
        function isNA(value) {
-              return value === error.na;
+              return value === _error['default'].na;
        }
 });
